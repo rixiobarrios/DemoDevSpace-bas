f8bc34fd3c771a9e78bf52bb43d2e8996a033046
Rixio Barrios <rixio.barrios@sap.com>
2023-06-07 15:49:52 +0000

    second commit

---
+++ demopodplugin-rixio/webapp/demopodplugin-rixio/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+	"sap/dm/dme/podfoundation/component/production/ProductionUIComponent",
+	"sap/ui/Device"
+], function (ProductionUIComponent, Device) {
+	"use strict";
+
+	return ProductionUIComponent.extend("sap.custom.rixio.demopodplugin.demopodplugin-rixio.demopodplugin-rixio.Component", {
+		metadata: {
+			manifest: "json"
+		}
+	});
+});
